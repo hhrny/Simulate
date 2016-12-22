71aa0a0e475196c474539adec7ce11765f8170ba
hhr <1016755439@qq.com>
2016-12-22 08:33:49 +0800

    hello world

--- compile.sh
+++ compile.sh
@@ -1,9 +1,1 @@
-find Simulate/src -name \*.java > javafile
-if [ -d Simulate/bin ]
-then
-    javac -cp ".:/lib/*" -d Simulate/bin @javafile
-else
-    mkdir Simulate/bin
-    javac -cp ".:/lib/*" -d Simulate/bin @javafile
-fi
-rm javafile
+hello world!
